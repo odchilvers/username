96360b5ae2974dc21059a16b152a9c0c495234ab
Olivia Chilvers <ochilvers@chapman.edu>
2020-11-13 11:33:13 -0800

    Adds username.sh

--- username.sh
+++ username.sh
@@ -2,11 +2,11 @@
 # username.sh
 # Olivia Chilvers
 echo "Enter a Username: "
-read NAME
-while echo "$NAME" | egrep -E -v "^[a-z]{5}$" > /dev/null 2>&1
+read -r NAME
+while echo "$NAME" | grep -E -v "^[a-z][0-9a-z_]{2,11}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid ZIP code - five digits only!"
-	echo "Enter a five-digit ZIP code: "
-	read NAME
+	echo "You must enter a valid Username - 3-12 characters, lowercase letters, digits,and underscores only!"
+	echo "Enter a username: "
+	read -r NAME
 done
 echo "Thank you"
